ae508adb7a7de07ae136f22b41adc4f0bc10031e
maxhall <maxwelljhall@gmail.com>
2017-08-08 00:05:05 +1000

    Using ES6 linting

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -2,7 +2,7 @@
 // An example Javascript module. Keep [clap] your [clap] code [clap] modular [clap]
 export default hello;
 
-var hello = function hello() {
-  var el = document.querySelectorAll('.welcome');
+const hello = function hello() {
+  const el = document.querySelectorAll('.welcome');
   el[0].textContent = 'Hello. Everything is in working order.';
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,11 @@
 import hello from './example';
 
 // You will want to set the page up as a pym child
-var pymChild = new pym.Child();
+const pymChild = new pym.Child();
 // Invoke the sendHeight method to notify the parent page our height has changed
 pymChild.sendHeight();
 
-var hi = () => { console.log('Babel is working too!') };
+const hi = () => { console.log('Babel is working too!'); };
 
 // Say hi
 hi();
